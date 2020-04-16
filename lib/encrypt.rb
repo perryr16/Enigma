1d5cdea1a15ea1ae1fe282c73d00c6e5062e607f
Ross Perry <perryr16@gmail.com>
2020-04-15 18:02:12 -0600

    reads file

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,15 +1,17 @@
 class Encrypt
 
-  # def txt_to_string_array
-  #   message = []
-  #    File.open("./lib/two_cards.txt").each do |line|
-  #     #File.open(@filename).each do |line|
-  #     message << line
-  #   end
-  #   card_list.each do |card|
-  #     card.slice!("\n")
-  #   end
-  #   #@card_list
-  # end
+  def initialize
+    @message = []
+  end
+
+  def read_txt
+    filename = "./text/secret_message.txt"
+    File.open(filename).each do |line|
+      @message << line
+    end
+    @message
+  end
+
+
 
 end
--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -7,8 +7,13 @@ require 'pry'
 class EncryptTest < Minitest::Test
 
   def test_it_exists
-    encryption = Encrypt.new
-    assert_instance_of Encrypt, encryption
+    encrypt = Encrypt.new
+    assert_instance_of Encrypt, encrypt
+  end
+
+  def test_it_returns_a_message_to_encrypt_from_txt
+    encrypt = Encrypt.new
+    assert_equal ["fly like an eagle\n"], encrypt.read_txt
   end
 
 end
