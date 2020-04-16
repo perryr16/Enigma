74e3229e08a617a2a0761d6cfad1f2472cc4ba58
Ross Perry <perryr16@gmail.com>
2020-04-16 17:08:03 -0600

    offsets and keys functional

--- lib/key_date.rb
+++ lib/key_date.rb
@@ -1,7 +1,24 @@
 class KeyDate
 
+  attr_reader :a_key,
+              :b_key,
+              :c_key,
+              :d_key,
+              :a_offset,
+              :b_offset,
+              :c_offset,
+              :d_offset
+              
   def today
     Date.today.strftime("%d%m%y").to_i
+  end
+
+  def offsets
+    offset = (today ** 2).digits[0..3].reverse
+    @a_offset = offset[0]
+    @b_offset = offset[1]
+    @c_offset = offset[2]
+    @d_offset = offset[3]
   end
 
   def five_digit
@@ -9,20 +26,11 @@ class KeyDate
     five_digit.to_s.rjust(5, "0")[-5..-1]
   end
 
-  def a_key
-    five_digit[0..1].to_i
-  end
-
-  def b_key
-    five_digit[1..2]
-  end
-
-  def c_key
-    five_digit[2..3]
-  end
-
-  def d_key
-    five_digit[3..4]
+  def keys
+    @a_key = five_digit[0..1].to_i
+    @b_key = five_digit[1..2].to_i
+    @c_key = five_digit[2..3].to_i
+    @d_key = five_digit[3..4].to_i
   end
 
 end
--- test/key_date_test.rb
+++ test/key_date_test.rb
@@ -18,8 +18,21 @@ class KeyDateTest < Minitest::Test
     key_date = KeyDate.new
 
     assert_equal 160420, key_date.today
+    Date.stubs(:today).returns(Date.new(1990, 10, 03))
+    assert_equal 31090, key_date.today
+    Date.stubs(:today).returns(Date.new(1555, 11, 23))
+    assert_equal 231155, key_date.today
+  end
+
+  def test_offests
+    key_date = KeyDate.new
     Date.stubs(:today).returns(Date.new(1991, 05, 13))
-    assert_equal 130591, key_date.today
+    #9281
+    key_date.offsets
+    assert_equal 9, key_date.a_offset
+    assert_equal 2, key_date.b_offset
+    assert_equal 8, key_date.c_offset
+    assert_equal 1, key_date.d_offset
   end
 
   def test_it_returns_a_5_digit_string
@@ -32,7 +45,11 @@ class KeyDateTest < Minitest::Test
   def test_keys
     key_date = KeyDate.new
     key_date.stubs(:five_digit).returns("01020")
+    key_date.keys
     assert_equal 1, key_date.a_key
+    assert_equal 10, key_date.b_key
+    assert_equal 2, key_date.c_key
+    assert_equal 20, key_date.d_key
   end
 
 end
