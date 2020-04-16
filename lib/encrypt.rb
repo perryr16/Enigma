e390fc688e1e1e9a91d18756beaad2eb3165b1a7
Ross Perry <perryr16@gmail.com>
2020-04-15 18:32:49 -0600

    split characters

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,15 +1,18 @@
 class Encrypt
 
+  attr_reader :message
   def initialize
     @message = []
   end
 
   def read_txt
     filename = "./text/secret_message.txt"
-    File.open(filename).each do |line|
-      @message << line
-    end
-    @message
+    File.open(filename).each {|line| @message << line}
+    @message.map {|line| line.slice!("\n")}
+  end
+
+  def split_characters
+    @message.join(" ").split("")
   end
 
 
--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -6,14 +6,23 @@ require 'pry'
 
 class EncryptTest < Minitest::Test
 
+  def setup
+    @encrypt = Encrypt.new
+    @encrypt.read_txt
+  end
+
   def test_it_exists
-    encrypt = Encrypt.new
-    assert_instance_of Encrypt, encrypt
+    assert_instance_of Encrypt, @encrypt
   end
 
   def test_it_returns_a_message_to_encrypt_from_txt
-    encrypt = Encrypt.new
-    assert_equal ["fly like an eagle\n"], encrypt.read_txt
+    assert_equal ["fly like an eagle", "if the dog is blue"], @encrypt.message
+
+  end
+
+  def test_it_returns_a_split_list_of_characters
+    expected = ["f", "l", "y", " ", "l", "i", "k", "e", " ", "a", "n", " ", "e", "a", "g", "l", "e", " ", "i", "f", " ", "t", "h", "e", " ", "d", "o", "g", " ", "i", "s", " ", "b", "l", "u", "e"]
+    assert_equal expected, @encrypt.split_characters
   end
 
 end
