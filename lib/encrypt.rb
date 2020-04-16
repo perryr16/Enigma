65c1f32962ecfcf3e600d52c2eeaf47d6d044277
Ross Perry <perryr16@gmail.com>
2020-04-16 08:22:33 -0600

    split code every 4th index

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,18 +1,30 @@
 class Encrypt
 
-  attr_reader :message
+  attr_reader :message, :a_code, :b_code, :c_code, :d_code
   def initialize
     @message = []
   end
 
   def read_txt
     filename = "./text/secret_message.txt"
-    File.open(filename).each {|line| @message << line}
+    File.open(filename).each {|line| @message << line.downcase}
     @message.map {|line| line.slice!("\n")}
   end
 
   def split_characters
     @message.join(" ").split("")
+  end
+
+
+
+  def split_4th_index
+    @a_code = []; @b_code = []; @c_code = []; @d_code = []
+    split_characters.each_with_index do |character, index|
+      @a_code << character if index % 4 == 0
+      @b_code << character if index % 4 == 1
+      @c_code << character if index % 4 == 2
+      @d_code << character if index % 4 == 3
+    end
   end
 
 
--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -9,6 +9,7 @@ class EncryptTest < Minitest::Test
   def setup
     @encrypt = Encrypt.new
     @encrypt.read_txt
+    @encrypt.split_4th_index
   end
 
   def test_it_exists
@@ -16,13 +17,21 @@ class EncryptTest < Minitest::Test
   end
 
   def test_it_returns_a_message_to_encrypt_from_txt
-    assert_equal ["fly like an eagle", "if the dog is blue"], @encrypt.message
+    assert_equal ["brown fox?", "sleepy dog! end"], @encrypt.message
 
   end
 
   def test_it_returns_a_split_list_of_characters
-    expected = ["f", "l", "y", " ", "l", "i", "k", "e", " ", "a", "n", " ", "e", "a", "g", "l", "e", " ", "i", "f", " ", "t", "h", "e", " ", "d", "o", "g", " ", "i", "s", " ", "b", "l", "u", "e"]
+    expected = ["b", "r", "o", "w", "n", " ", "f", "o", "x", "?", " ", "s", "l", "e", "e", "p", "y", " ", "d", "o", "g", "!", " ", "e", "n", "d"]
     assert_equal expected, @encrypt.split_characters
+  end
+
+  def test_it_creates_arrays_every_4th_position
+
+    assert_equal ["b", "n", "x", "l", "y", "g", "n"], @encrypt.a_code
+    assert_equal ["r", " ", "?", "e", " ", "!", "d"], @encrypt.b_code
+    assert_equal ["o", "f", " ", "e", "d", " "], @encrypt.c_code
+    assert_equal ["w", "o", "s", "p", "o", "e"], @encrypt.d_code
   end
 
 end
