67b2c375617189222f950e50a4aef1f703105021
Ross Perry <perryr16@gmail.com>
2020-04-17 14:47:11 -0600

    moved encrypt and decrypt methods to child class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,15 +2,20 @@ require_relative 'encrypt'
 
 class Enigma < Encrypt
 
-  def initialize
+  def encrypt(message, key = nil, date = today)
+    shifts(key, date)
+    numbers = zip_together(message, a_shift, b_shift, c_shift, d_shift)
+    encrypted = to_alpha(numbers)
+    {encryption: encrypted, key: key, date: date.to_s}
 
   end
 
-  # def encrypt(message, key = nil, date = today)
-  #
-  # end
-  #
-  # def decrypt(message, key = nil, date = today)
-  #
-  # end
+  def decrypt(code, key = nil, date = today)
+      shifts(key, date)
+      to_numeric(code)
+      split_4th(code)
+      numbers = de_zip_together(code, a_shift, b_shift, c_shift, d_shift)
+      decrypted = to_alpha(numbers)
+      {decryption: decrypted, key: key, date: date.to_s}
+  end
 end
