0202a9315340a968b159d85de55c0272fc1febf9
Ross Perry <perryr16@gmail.com>
2020-04-16 18:50:33 -0600

    refacot for dynamics

--- lib/key_date.rb
+++ lib/key_date.rb
@@ -8,21 +8,21 @@ class KeyDate
               :d_shift
 
   def initialize
-    keys
-    offsets
-    shifts
   end
 
   def today
     Date.today.strftime("%d%m%y").to_i
   end
 
-  def offsets
-    offset = (today ** 2).digits[0..3].reverse
-    a_offset = offset[0]
-    b_offset = offset[1]
-    c_offset = offset[2]
-    d_offset = offset[3]
+  def date_squared(date)
+    (date ** 2).digits[0..3].reverse
+  end
+
+  def offsets(date_squared)
+    a_offset = date_squared[0]
+    b_offset = date_squared[1]
+    c_offset = date_squared[2]
+    d_offset = date_squared[3]
     @offsets = [a_offset, b_offset, c_offset, d_offset]
   end
 
@@ -31,19 +31,22 @@ class KeyDate
     five_digit.to_s.rjust(5, "0")[-5..-1]
   end
 
-  def keys
-    a_key = five_digit[0..1].to_i
-    b_key = five_digit[1..2].to_i
-    c_key = five_digit[2..3].to_i
-    d_key = five_digit[3..4].to_i
+  def keys(number)
+    a_key = number[0..1].to_i
+    b_key = number[1..2].to_i
+    c_key = number[2..3].to_i
+    d_key = number[3..4].to_i
     [a_key, b_key, c_key, d_key]
   end
 
-  def shifts
-    @a_shift = keys[0] + offsets[0]
-    @b_shift = keys[1] + offsets[1]
-    @c_shift = keys[2] + offsets[2]
-    @d_shift = keys[3] + offsets[3]
+  def shifts(number, date)
+    offset = offsets(date_squared(date))
+    key = keys(number) if !number.nil?
+    key = keys(five_digit) if number.nil?
+    @a_shift = key[0] + offset[0]
+    @b_shift = key[1] + offset[1]
+    @c_shift = key[2] + offset[2]
+    @d_shift = key[3] + offset[3]
   end
 
 end
--- test/key_date_test.rb
+++ test/key_date_test.rb
@@ -24,15 +24,22 @@ class KeyDateTest < Minitest::Test
     assert_equal 231155, key_date.today
   end
 
+  def test_date_squared
+    key_date = KeyDate.new
+    Date.stubs(:today).returns(Date.new(1991, 05, 13))
+    #9281
+    assert_equal [9,2,8,1], key_date.date_squared
+  end
+
   def test_offests
     key_date = KeyDate.new
     Date.stubs(:today).returns(Date.new(1991, 05, 13))
     #9281
-    key_date.offsets
-    assert_equal 9, key_date.offsets[0]
-    assert_equal 2, key_date.offsets[1]
-    assert_equal 8, key_date.offsets[2]
-    assert_equal 1, key_date.offsets[3]
+    date_squared = key_date.date_squared(key_date.today)
+    assert_equal 9, key_date.offsets(date_squared)[0]
+    assert_equal 2, key_date.offsets(date_squared)[1]
+    assert_equal 8, key_date.offsets(date_squared)[2]
+    assert_equal 1, key_date.offsets(date_squared)[3]
   end
 
   def test_it_returns_a_5_digit_string
@@ -45,25 +52,37 @@ class KeyDateTest < Minitest::Test
   def test_keys
     key_date = KeyDate.new
     key_date.stubs(:five_digit).returns("01020")
-    key_date.keys
-    assert_equal 1, key_date.keys[0]
-    assert_equal 10, key_date.keys[1]
-    assert_equal 2, key_date.keys[2]
-    assert_equal 20, key_date.keys[3]
+    number = key_date.five_digit
+    assert_equal 1, key_date.keys(number)[0]
+    assert_equal 10, key_date.keys(number)[1]
+    assert_equal 2, key_date.keys(number)[2]
+    assert_equal 20, key_date.keys(number)[3]
   end
 
   def test_shifts
     key_date = KeyDate.new
     key_date.stubs(:five_digit).returns("01020")
     Date.stubs(:today).returns(Date.new(1991, 05, 13))
-    key_date.keys
-    key_date.offsets
-    key_date.shifts
+    number = key_date.five_digit
+
+    key_date.shifts(number, key_date.today)
+    assert_equal 10, key_date.a_shift
+    assert_equal 12, key_date.b_shift
+    assert_equal 10, key_date.c_shift
+    assert_equal 21, key_date.d_shift
+
+    key_date.shifts(nil, key_date.today)
     assert_equal 10, key_date.a_shift
     assert_equal 12, key_date.b_shift
     assert_equal 10, key_date.c_shift
     assert_equal 21, key_date.d_shift
   end
+
+  # def test_case_name
+  #   key_date = KeyDate.new
+  #
+  #   binding.pry
+  # end
 
 
 end
