11bc09dd48a6a1ac76cff4b12b716977a51ed31f
Ross Perry <perryr16@gmail.com>
2020-04-17 14:45:50 -0600

    tests modified for parent class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,16 +2,15 @@ require_relative 'encrypt'
 
 class Enigma < Encrypt
 
-  attr_reader :encrypter
   def initialize
 
   end
 
-  def encrypt(message, key = nil, date = today)
-
-  end
-
-  def decrypt(message, key = nil, date = today)
-
-  end
+  # def encrypt(message, key = nil, date = today)
+  #
+  # end
+  #
+  # def decrypt(message, key = nil, date = today)
+  #
+  # end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -17,6 +17,7 @@ class EnigmaTest < Minitest::Test
 
   def test_it_encrypts_message
     enigma =  Enigma.new
+    # binding.pry
     expected = {:encryption => "v rw", :key=>"12345", :date=>"130591"}
     assert_equal expected, enigma.encrypt("abcd", "12345", "130591")
     expected = {:encryption => "v rw!!!", :key=>"12345", :date=>"130591"}
@@ -25,17 +26,17 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, enigma.encrypt("abcd", "12345")
     assert_equal true, enigma.encrypt("abcd").is_a?(Hash)
     assert_equal true, enigma.encrypt("abcd")[:encryption].length == 4
-    enigma.encrypter.stubs(:a_shift).returns(1)
-    enigma.encrypter.stubs(:b_shift).returns(2)
-    enigma.encrypter.stubs(:c_shift).returns(3)
-    enigma.encrypter.stubs(:d_shift).returns(4)
+    enigma.stubs(:a_shift).returns(1)
+    enigma.stubs(:b_shift).returns(2)
+    enigma.stubs(:c_shift).returns(3)
+    enigma.stubs(:d_shift).returns(4)
     expected = {:encryption=>"ctr obisy?cwmghtzbgsh!ciof", :key=>nil, :date=>"170420"}
     assert_equal expected, enigma.encrypt("brown fox? sleepy dog! end")
   end
 
   def test_it_decrypts_message
     enigma =  Enigma.new
-    
+
     expected = {:decryption=>"abcd", :key=>"12345", :date=>"130591"}
     assert_equal expected, enigma.decrypt("v rw", "12345", "130591")
     expected = {:decryption=>"abcd", :key=>"12345", :date=>"170420"}
