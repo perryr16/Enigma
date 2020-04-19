2b6d825587aeda729c88e21613331f2092c075eb
Ross Perry <perryr16@gmail.com>
2020-04-19 16:53:05 -0600

    crack produces correct key

--- lib/crack.rb
+++ lib/crack.rb
@@ -1,9 +1,9 @@
 class Crack
 
-  attr_reader :crack_shifts
+  attr_reader :cracked_key
   def initialize
     @alpha_num = AlphaNum.new
-    # @shift_gen = ShiftGen.new
+    @shift_gen = ShiftGen.new
   end
 
   def delta_end(message)
@@ -21,7 +21,66 @@ class Crack
     a_index = 4 if a_index == 0
     crack_keys = [:a, :b, :c, :d].rotate(a_index)
     crack_vals = [delta[:space], delta[:e], delta[:n], delta[:d]]
-    @crack_shifts = Hash[crack_keys.zip(crack_vals)]
+    Hash[crack_keys.zip(crack_vals)]
+  end
+
+  # def crack_key(message, date)
+  #   offset = @shift_gen.offsets(date)
+  #   crack_a_key = (crack_shift(message)[:a] - offset[0]) #12
+  #   crack_b_key = (crack_shift(message)[:b] - offset[1]) + 27 #23
+  #   crack_c_key = (crack_shift(message)[:c] - offset[2]) + 27 #34
+  #   crack_d_key = (crack_shift(message)[:d] - offset[3]) + 27 #45
+  #   @key = crack_a_key.to_s[0]+crack_b_key.to_s[0]+crack_c_key.to_s[0]+crack_d_key.to_s.rjust(2, "0")
+  #   binding.pry
+  # end
+  #
+  # def crack_key_new(message, date)
+  #   offset = @shift_gen.offsets(date)
+  #   crack_a_key = (crack_shift(message)[:a] - offset[0]) #12
+  #   crack_b_key = all_possible_keys(message, date, :b, 1).find {}
+  # end
+
+  def all_possible_keys(message, date, letter_sym, offset_position)
+    offset = @shift_gen.offsets(date)
+    possible_keys = []
+    index = 0
+    loop do
+      possible_key = (crack_shift(message)[letter_sym] - offset[offset_position]) + 27*index  #23
+      break if possible_key > 99
+      possible_keys << possible_key.to_s.rjust(2, "0") if possible_key > 0
+      index += 1
+    end
+    possible_keys
+  end
+
+  def the_key(message, date, letter_sym, offset_position, first_digit)
+    all_possible_keys(message, date, letter_sym, offset_position).find do |key|
+      key[0] == first_digit
+    end.to_s
+  end
+
+  def a_keys(message, date)
+    all_possible_keys(message, date, :a, 0)
+  end
+
+  def four_good_keys(message, date)
+    the_keys = []
+    a_keys(message, date).each_with_index do |key|
+      the_keys = [key.to_s]
+      # binding.pry
+      the_keys << the_key(message, date, :b, 1, key.to_s[1])
+      # binding.pry
+      the_keys << the_key(message, date, :c, 2, the_keys.last[1])
+      # binding.pry
+      the_keys << the_key(message, date, :d, 3, the_keys.last[1])
+      return the_keys if !the_keys.include?("")
+    end
+    the_keys
+  end
+
+  def cracked_key(message, date)
+    four_keys = four_good_keys(message, date)
+    @cracked_key = four_keys[0][0] + four_keys[1][0] + four_keys[2][0] + four_keys[3]
   end
 
 end
--- test/crack_test.rb
+++ test/crack_test.rb
@@ -6,6 +6,7 @@ require 'pry'
 require 'mocha/minitest'
 require './lib/crack'
 require './lib/alpha_num'
+require './lib/shift_gen'
 
 
 class CrackTest < Minitest::Test
@@ -57,10 +58,57 @@ class CrackTest < Minitest::Test
     assert_equal expected, crack.crack_shift("x fpg")
     expected = {:a=>2, :b=>3, :c=>0, :d=>1}
     assert_equal expected, crack.crack_shift("xx fpg")
-    assert_equal expected, crack.crack_shifts
+    # assert_equal expected, crack.crack_shifts
     expected = {:a=>1, :b=>2, :c=>3, :d=>0}
     assert_equal expected, crack.crack_shift("xxx fpg")
-    assert_equal expected, crack.crack_shifts
+    # assert_equal expected, crack.crack_shifts
+  end
+
+  def test_all_possible_keys
+    crack = Crack.new
+    assert_equal ["18", "45", "72", "99"], crack.all_possible_keys("x ev","190420", :a, 0)
+    assert_equal ["18", "45", "72", "99"], crack.all_possible_keys("x ev","190420", :b, 1)
+    assert_equal ["18", "45", "72", "99"], crack.all_possible_keys("x ev","190420", :c, 2)
+    assert_equal ["18", "45", "72", "99"], crack.all_possible_keys("x ev","190420", :d, 3)
+
+    assert_equal ["12", "39", "66", "93"], crack.all_possible_keys("reuv","190420", :a, 0)
+    assert_equal ["23", "50", "77"], crack.all_possible_keys("reuv","190420", :b, 1)
+    assert_equal ["07", "34", "61", "88"], crack.all_possible_keys("reuv","190420", :c, 2)
+    assert_equal ["18", "45", "72", "99"], crack.all_possible_keys("reuv","190420", :d, 3)
+  end
+
+  def test_a_keys
+    crack = Crack.new
+    assert_equal ["18", "45", "72", "99"], crack.a_keys("x ev","190420")
+    assert_equal ["12", "39", "66", "93"], crack.a_keys("reuv","190420")
+
+  end
+
+  def test_the_key
+    crack = Crack.new
+    assert_equal "23", crack.the_key("reuv","190420", :b, 1, "2")
+    assert_equal "34", crack.the_key("reuv","190420", :c, 2, "3")
+    assert_equal "45", crack.the_key("reuv","190420", :d, 3, "4")
+
+    assert_equal "99", crack.the_key("x ev","190420", :b, 1, "9")
+    assert_equal "99", crack.the_key("x ev","190420", :c, 2, "9")
+    assert_equal "99", crack.the_key("x ev","190420", :d, 3, "9")
+  end
+
+  def test_four_good_keys
+    crack = Crack.new
+    assert_equal ["12", "23", "34", "45"], crack.four_good_keys("reuv", "190420")
+    assert_equal ["99", "99", "99", "99"], crack.four_good_keys("x ev", "190420")
+    assert_equal ["01", "10", "01", "10"], crack.four_good_keys("gson", "190420")
+    assert_equal ["99", "99", "99", "99"], crack.four_good_keys("x ev", "190420")
+  end
+
+  def test_cracked_key
+    crack = Crack.new
+    assert_equal "12345", crack.cracked_key("reuv", "190420")
+    assert_equal "99999", crack.cracked_key("x ev", "190420")
+    assert_equal "01010", crack.cracked_key("gson", "190420")
+    assert_equal "86593", crack.cracked_key("ktsp", "190420")
   end
 
 
