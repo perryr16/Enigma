fee855791e19e4ba4ad2636d721097be14f6e0e4
Ross Perry <perryr16@gmail.com>
2020-04-19 16:59:30 -0600

    test to produce crack data

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -4,6 +4,8 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/encryption_algorithm'
 require './lib/enigma'
+# require './lib/alpha_num'
+# require './lib/shift_gen'
 require 'mocha/minitest'
 
 require 'pry'
@@ -84,6 +86,18 @@ class EnigmaTest < Minitest::Test
 
     expected = enigma.read_txt("./text/test_decrypted.txt")[0]
     assert_equal expected, enigma.decrypted_details[:decryption]
+  end
+
+  def test_for_me
+    enigma = Enigma.new
+    expected = {:encryption=>"reuv", :key=>"12345", :date=>"190420"}
+    assert_equal expected, enigma.encrypt(" end", "12345")
+    expected = {:encryption=>"x ev", :key=>"99999", :date=>"190420"}
+    assert_equal expected, enigma.encrypt(" end", "99999")
+    expected = {:encryption=>"gson", :key=>"01010", :date=>"190420"}
+    assert_equal expected, enigma.encrypt(" end", "01010")
+    expected = {:encryption=>"ktsp", :key=>"86593", :date=>"190420"}
+    assert_equal expected, enigma.encrypt(" end", "86593")
   end
 
 end
