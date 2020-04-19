698a0c796492c402184b6ac0cf9cf93b5a5c4fe2
Ross Perry <perryr16@gmail.com>
2020-04-19 12:30:21 -0600

    crack will identify shift amount from . end

--- lib/crack.rb
+++ lib/crack.rb
@@ -1,67 +1,27 @@
 class Crack
 
+  attr_reader :crack_shifts
   def initialize
     @alpha_num = AlphaNum.new
     # @shift_gen = ShiftGen.new
   end
 
-  # def delta_end(message)
-  #   delta_end = {}
-  #   delta_end[:space] = (@alpha_num.a_one[message[-4]] - 26) % 27
-  #   delta_end[:e] = (@alpha_num.a_one[message[-3]] - 4) % 27
-  #   delta_end[:n] = (@alpha_num.a_one[message[-2]] - 13) % 27
-  #   delta_end[:d] = (@alpha_num.a_one[message[-1]] - 3) % 27
-  #   delta_end
-  # end
-
-  # def crack_a_index(message)
-  #   return -4 if message.length % 4 == 0
-  #   return -3 if message.length % 4 == 1
-  #   return -2 if message.length % 4 == 2
-  #   return -1 if message.length % 4 == 3
-  # end
-
-  # def crack_indexes(message)
-  #   a_index = (message.length % 4)
-  #   b_index = (a_index + 3) % 4
-  #   c_index = (a_index + 2) % 4
-  #   d_index = (a_index + 1) % 4
-  #   crack_indexes = [a_index, b_index, c_index, d_index]
-  #   crack_indexes[crack_indexes.index(0)] =  4
-  #   crack_indexes = crack_indexes.map {|val| val * -1}
-  #   crack_indexes = {a: crack_indexes[0], b: crack_indexes[1], c: crack_indexes[2], d: crack_indexes[3]}
-  # end
+  def delta_end(message)
+    delta_end = {}
+    delta_end[:space] = (@alpha_num.a_one[message[-4]] - 26) % 27
+    delta_end[:e] = (@alpha_num.a_one[message[-3]] - 4) % 27
+    delta_end[:n] = (@alpha_num.a_one[message[-2]] - 13) % 27
+    delta_end[:d] = (@alpha_num.a_one[message[-1]] - 3) % 27
+    delta_end
+  end
 
   def crack_shift(message)
-    crack_shift = {a: 0, b: 0, c: 0, d: 0}
-    a_index = (message.length % -4)
-    a_index = -4 if a_index == 0
-    if a_index == -4
-      # space
-      crack_shift[:a] = (@alpha_num.a_one[message[-4]] - 26) % 27
-      # e
-      crack_shift[:b] = (@alpha_num.a_one[message[-3]] - 4) % 27
-      # n
-      crack_shift[:c] = (@alpha_num.a_one[message[-2]] - 13) % 27
-      # d
-      crack_shift[:d] = (@alpha_num.a_one[message[-1]] - 3) % 27
-    elsif a_index == -3
-      crack_shift[:d] = (@alpha_num.a_one[message[-4]] - 26) % 27
-      crack_shift[:a] = (@alpha_num.a_one[message[-3]] - 4) % 27
-      crack_shift[:b] = (@alpha_num.a_one[message[-2]] - 13) % 27
-      crack_shift[:c] = (@alpha_num.a_one[message[-1]] - 3) % 27
-    elsif a_index == -2
-      crack_shift[:c] = (@alpha_num.a_one[message[-4]] - 26) % 27
-      crack_shift[:d] = (@alpha_num.a_one[message[-3]] - 4) % 27
-      crack_shift[:a] = (@alpha_num.a_one[message[-2]] - 13) % 27
-      crack_shift[:b] = (@alpha_num.a_one[message[-1]] - 3) % 27
-    elsif a_index == -1
-      crack_shift[:b] = (@alpha_num.a_one[message[-4]] - 26) % 27
-      crack_shift[:c] = (@alpha_num.a_one[message[-3]] - 4) % 27
-      crack_shift[:d] = (@alpha_num.a_one[message[-2]] - 13) % 27
-      crack_shift[:a] = (@alpha_num.a_one[message[-1]] - 3) % 27
-    end
-    crack_shift
+    delta = delta_end(message)
+    a_index = (message.length % 4)
+    a_index = 4 if a_index == 0
+    crack_keys = [:a, :b, :c, :d].rotate(a_index)
+    crack_vals = [delta[:space], delta[:e], delta[:n], delta[:d]]
+    @crack_shifts = Hash[crack_keys.zip(crack_vals)]
   end
 
 end
--- test/crack_test.rb
+++ test/crack_test.rb
@@ -15,17 +15,17 @@ class CrackTest < Minitest::Test
     assert_instance_of Crack, crack
   end
 
-  def test_it_returns_delta_end
-    crack = Crack.new
-    expected = {space: 0, e: 0, n: 0, d: 0}
-    assert_equal expected, crack.delta_end(" end")
-    expected = {space: 1, e: 1, n: 1, d: 1}
-    assert_equal expected, crack.delta_end("afoe")
-    expected = {space: 1, e: 2, n: 3, d: 4}
-    assert_equal expected, crack.delta_end("agqh")
-    expected = {space: 26, e: 26, n: 26, d: 26}
-    assert_equal expected, crack.delta_end("zdmc")
-  end
+  # def test_it_returns_delta_end
+  #   crack = Crack.new
+  #   expected = {space: 0, e: 0, n: 0, d: 0}
+  #   assert_equal expected, crack.delta_end(" end")
+  #   expected = {space: 1, e: 1, n: 1, d: 1}
+  #   assert_equal expected, crack.delta_end("afoe")
+  #   expected = {space: 1, e: 2, n: 3, d: 4}
+  #   assert_equal expected, crack.delta_end("agqh")
+  #   expected = {space: 26, e: 26, n: 26, d: 26}
+  #   assert_equal expected, crack.delta_end("zdmc")
+  # end
   #
   # def test_crack_a_index
   #   crack = Crack.new
@@ -36,23 +36,31 @@ class CrackTest < Minitest::Test
   #   assert_equal -4, crack.crack_a_index("xxxx end")
   # end
 
-  def test_crack_indexes
-    crack = Crack.new
-    expected = {a: -4, b: -3, c: -2, d: -1}
-    assert_equal expected, crack.crack_indexes(" end")
-    assert_equal expected, crack.crack_indexes("xxxx end")
-    expected = {a: -1, b: -4, c: -3, d: -2}
-    assert_equal expected, crack.crack_indexes("x end")
-    expected = {a: -2, b: -1, c: -4, d: -3}
-    assert_equal expected, crack.crack_indexes("xx end")
-    expected = {a: -3, b: -2, c: -1, d: -4}
-    assert_equal expected, crack.crack_indexes("xxx end")
-  end
+  # def test_crack_indexes
+  #   crack = Crack.new
+  #   expected = {a: -4, b: -3, c: -2, d: -1}
+  #   assert_equal expected, crack.crack_indexes(" end")
+  #   assert_equal expected, crack.crack_indexes("xxxx end")
+  #   expected = {a: -1, b: -4, c: -3, d: -2}
+  #   assert_equal expected, crack.crack_indexes("x end")
+  #   expected = {a: -2, b: -1, c: -4, d: -3}
+  #   assert_equal expected, crack.crack_indexes("xx end")
+  #   expected = {a: -3, b: -2, c: -1, d: -4}
+  #   assert_equal expected, crack.crack_indexes("xxx end")
+  # end
 
   def test_crack_shift
     crack = Crack.new
-    binding.pry
-    crack.crack_shift("xaend")
+    expected = {:a=>0, :b=>1, :c=>2, :d=>3}
+    assert_equal expected, crack.crack_shift(" fpg")
+    expected = {:a=>3, :b=>0, :c=>1, :d=>2}
+    assert_equal expected, crack.crack_shift("x fpg")
+    expected = {:a=>2, :b=>3, :c=>0, :d=>1}
+    assert_equal expected, crack.crack_shift("xx fpg")
+    assert_equal expected, crack.crack_shifts
+    expected = {:a=>1, :b=>2, :c=>3, :d=>0}
+    assert_equal expected, crack.crack_shift("xxx fpg")
+    assert_equal expected, crack.crack_shifts
   end
 
 
