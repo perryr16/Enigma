b9feb11cfd48db32d573113b86e1541e4e52d6ac
Ross Perry <perryr16@gmail.com>
2020-04-20 10:10:10 -0600

    add module methods to test

--- test/crack_test.rb
+++ test/crack_test.rb
@@ -87,4 +87,26 @@ class CrackTest < Minitest::Test
     assert_equal "12345", crack.cracked_key("khlrvelireuafygwstpey i jca ks,rsnkrkhlruocjrcherdheueg x  zwygnsn rkogwed", "190420")
   end
 
+
+    def test_it_creates_a_hash_of_aplha_to_numbers
+      crack = Crack.new
+
+      expected = {"a"=>0, "b"=>1, "c"=>2, "d"=>3, "e"=>4, "f"=>5, "g"=>6, "h"=>7,
+                  "i"=>8, "j"=>9, "k"=>10, "l"=>11, "m"=>12, "n"=>13, "o"=>14,
+                  "p"=>15, "q"=>16, "r"=>17, "s"=>18, "t"=>19, "u"=>20, "v"=>21,
+                  "w"=>22, "x"=>23, "y"=>24, "z"=>25, " "=>26}
+                  # binding.pry
+      assert_equal expected, crack.a_one
+    end
+
+    def test_it_creaters_a_hash_of_numeric_to_alpha
+      crack = Crack.new
+      
+      expected = {0=>"a", 1=>"b", 2=>"c", 3=>"d", 4=>"e", 5=>"f", 6=>"g", 7=>"h",
+                  8=>"i", 9=>"j", 10=>"k", 11=>"l", 12=>"m", 13=>"n", 14=>"o",
+                  15=>"p", 16=>"q", 17=>"r", 18=>"s", 19=>"t", 20=>"u", 21=>"v",
+                  22=>"w", 23=>"x", 24=>"y", 25=>"z", 26=>" "}
+      assert_equal expected, crack.one_a
+    end
+
 end
